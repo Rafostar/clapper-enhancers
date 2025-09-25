8381bd52213a0c62195ec3644e299e6e08b54c1d
Rafał Dzięgiel <rafostar.github@gmail.com>
2025-09-25 18:31:06 +0200

    meson: Versioning for git builds
    When building from untagged or dirty git repo, extend version
    information with git commit timestamp and SHA. If dirty, use
    current timestamp and also append "-dirty" suffix.
    
    This helps to determine which exact commit is used and ensures that
    enhancers cache is always rebuild during development for easier testing.

---
+++ src/gen-version.py
@@ -0,0 +1,39 @@
+#
+# Copyright (C) 2025 Rafał Dzięgiel <rafostar.github@gmail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see
+# <https://www.gnu.org/licenses/>.
+
+import datetime, subprocess, sys
+
+template = open(sys.argv[1]).read()
+version = sys.argv[2]
+
+try:
+    tagged = subprocess.check_output(["git", "tag", "--points-at", "HEAD"]).decode().split()
+    dirty = subprocess.check_output(["git", "describe", "--dirty"]).decode().strip().endswith("-dirty")
+    if not tagged or dirty:
+        if dirty:
+            timestamp = int(datetime.datetime.utcnow().timestamp())
+        else:
+            timestamp = subprocess.check_output(["git", "log", "-1", "--format=%ct"]).decode().strip()
+        sha = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode().strip()
+        version += f"+git.{timestamp}~{sha}"
+        if dirty:
+            version += "-dirty"
+except Exception:
+    pass
+
+out = template.replace("@VERSION@", version)
+sys.stdout.write(out)
