213f2fe908f0e55f33ea31684740d2eabea45c10
Rafał Dzięgiel <rafostar.github@gmail.com>
2025-12-05 19:27:46 +0100

    yt-dlp: overrides: Compatibility changes for recent yt-dlp

--- src/yt-dlp/clapper_yt_dlp_overrides.py
+++ src/yt-dlp/clapper_yt_dlp_overrides.py
@@ -15,10 +15,14 @@
 # License along with this library; if not, see
 # <https://www.gnu.org/licenses/>.
 
-import re
+import re, gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst
 
 from yt_dlp.extractor.youtube import YoutubeIE
-from yt_dlp.utils import parse_m3u8_attributes
+from yt_dlp.utils import parse_m3u8_attributes, traverse_obj
+
+import clapper_yt_dlp_debug as debug
 
 BLACKLIST = [
     'Youtube'
@@ -31,8 +35,8 @@ class ClapperYoutubeIE(YoutubeIE):
 
     _ITAG_PATTERN = re.compile(r'/itag/(\d+)')
 
-    def _extract_formats_and_subtitles(self, streaming_data, video_id, player_url, live_status, duration):
-        *formats, subtitles = super()._extract_formats_and_subtitles(streaming_data, video_id, player_url, live_status, duration)
+    def _extract_formats_and_subtitles(self, video_id, player_responses, player_url, live_status, duration):
+        *formats, subtitles = super()._extract_formats_and_subtitles(video_id, player_responses, player_url, live_status, duration)
 
         for fmt in formats:
             if not (format_id := fmt.get('format_id')):
@@ -41,10 +45,18 @@ class ClapperYoutubeIE(YoutubeIE):
             itag = format_id.split('-')[0]
             lang = (language := fmt.get('language')) and language.split('-')[0]
 
-            for resp_data in streaming_data:
+            debug.print_leveled(Gst.DebugLevel.DEBUG, f'Processing format ID: {format_id} (itag: {itag})')
+
+            for pr in player_responses:
+                if not (
+                        (resp_data := traverse_obj(pr, 'streamingData'))
+                        and (adapt_formats := resp_data.get('adaptiveFormats'))
+                ):
+                    continue
+
                 resp_found = False
 
-                for yt_fmt in resp_data.get('adaptiveFormats'):
+                for yt_fmt in adapt_formats:
                     # Find format with the same itag
                     if (
                             itag != str(yt_fmt.get('itag') or 0)
@@ -58,6 +70,8 @@ class ClapperYoutubeIE(YoutubeIE):
                     # Youtube uses the same itag for different languages, so language must also match
                     if (lang != yt_lang):
                         continue
+
+                    debug.print_leveled(Gst.DebugLevel.DEBUG, 'Found matching YT format')
 
                     init_range = yt_fmt.get('initRange')
                     index_range = yt_fmt.get('indexRange')
@@ -78,6 +92,7 @@ class ClapperYoutubeIE(YoutubeIE):
                         'init_range': f'{init_start}-{init_end}',
                         'index_range': f'{index_start}-{index_end}'
                     }
+                    debug.print_leveled(Gst.DebugLevel.DEBUG, 'Added format streaming options')
 
                     resp_found = True
                     break
